034c132449e119c118279d2e1f4915e9a85b14cf
Leonardo <lpbaiser@gmail.com>
2016-12-01 22:49:32 -0200

    arquivo

--- Extractor/src/main/java/com/extractor/Map.java
+++ Extractor/src/main/java/com/extractor/Map.java
@@ -10,7 +10,6 @@ public class Map extends Mapper<Object, Text, Text, IntWritable> {
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
-
     @Override
     public void map(Object key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         StringTokenizer itr = new StringTokenizer(value.toString());
