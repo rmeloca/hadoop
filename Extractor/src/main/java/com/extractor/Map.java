8c74add77e1771dc5a2ed2a7f415262c0595cfa9
rmeloca <rmeloca@gmail.com>
2016-12-01 23:22:10 -0200

    eita

--- Extractor/src/main/java/com/extractor/Map.java
+++ Extractor/src/main/java/com/extractor/Map.java
@@ -1,22 +1,32 @@
 package com.extractor;
 
+import info.debatty.java.stringsimilarity.Jaccard;
 import java.io.IOException;
-import java.util.StringTokenizer;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class Map extends Mapper<Object, Text, Text, IntWritable> {
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+    Text tweet1 = null;
+    Text tweet2 = null;
+    IntWritable distance = null;
 
     @Override
-    public void map(Object key, Text value, Mapper.Context context) throws IOException, InterruptedException {
-        StringTokenizer itr = new StringTokenizer(value.toString());
-        while (itr.hasMoreTokens()) {
-            word.set(itr.nextToken());
-            context.write(word, one);
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        Jaccard jaccard = new Jaccard(2);
+        String[] split = value.toString().split(":");
+        String tweet = split[1];
+
+        if (tweet1 != null && tweet2 != null && distance != null) {
+            System.out.println(tweet);
+            jaccard.similarity("ABCDE", "ABCDF");
+            this.tweet1 = new Text(split[1]);
+            this.distance = new IntWritable(Integer.valueOf(split[2]));
+            context.write(new Text(tweet1 + "\t" + tweet2), distance);
+            tweet1 = null;
+            tweet2 = null;
+            distance = null;
         }
     }
 }
